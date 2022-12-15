c5db79b9f78e99c924f14f2a0951a5bfc0b2f9e6
Vasyl Klishch <klishchjob.gmail.com>
2022-12-15 18:17:14 +0200

    робота з функціями

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
-const numberOfFilms = +prompt('How many movies did you watch?','')
-const lastWatchedMovie_1 = prompt('What movie did you last watch?','')
-const markMovie_1 = prompt('Please enter its mark','')
-const lastWatchedMovie_2 = prompt('What movie did you last watch?','')
-const markMovie_2 = prompt('Please enter its mark','')
+"use strict"
+let numberOfFilms,
+    userStatus = '',
+    genre,
+    i = 1
+
+
+function start(){
+    numberOfFilms = +prompt('How many movies did you watch?','')
+
+    while(numberOfFilms === '' || numberOfFilms == null || isNaN(numberOfFilms) || numberOfFilms === undefined || numberOfFilms === 0){
+        numberOfFilms = +prompt('How many movies did you watch?','')
+
+    }
+
+}
+function personalRaiting(){
+    if(personalMovieDB.count < 10){
+        userStatus = 'переглянуто досить мало фільмів'
+
+    }else if(personalMovieDB.count >10 && personalMovieDB.count <= 30){
+        userStatus = 'Ви середній глядач'
+    }else if(personalMovieDB.count > 30){
+        userStatus = 'Ви кіноман'
+    }else{
+        console.log('error')
+    }
+
+}
+function showMyDB(){
+    if(!personalMovieDB.private){
+        console.log(personalMovieDB)
+    }
+}
+function writeYourGenres(){
+    for(let i = 1; i <=3; i++){
+        genre = prompt('Please enter your favourite genres', '')
+        if(i < 1)
+            i = 1;
+        if(Number.isInteger(genre) || genre == null){
+            i--;
+        }else{
+            personalMovieDB.genres.push(genre)
+
+        }
+    }
+}
+start()
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,7 +54,23 @@ const personalMovieDB = {
     genres: [],
     private: false,
 };
-personalMovieDB.movies[lastWatchedMovie_1] = markMovie_1
-personalMovieDB.movies[lastWatchedMovie_2] = markMovie_2
 
-console.log(personalMovieDB)
+
+do{
+    let lastWatchedMovie = prompt('What movie did you last watch?','')
+    let markMovie = prompt('Please enter its mark', '');
+
+    if(lastWatchedMovie != null && markMovie !== null && lastWatchedMovie !== '' && lastWatchedMovie.length <=  50 && markMovie !== ''){
+        personalMovieDB.movies[lastWatchedMovie] = markMovie
+        console.log('saved movie')
+        i++
+    }else{
+        i--
+        if(i < 1) i = 1
+    }
+}while (i <= 2)
+
+
+personalRaiting()
+showMyDB()
+writeYourGenres()
