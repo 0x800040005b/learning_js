9d95751ec6f866da6c35518c74d0e7a1d3ac6461
Vasyl Klishch <klishchjob.gmail.com>
2023-01-03 23:07:19 +0200

    iterated object

--- index.js
+++ index.js
@@ -1,76 +1,48 @@
 "use strict"
-let numberOfFilms,
-    userStatus = '',
-    genre,
-    i = 1
 
-
-function start(){
-    numberOfFilms = +prompt('How many movies did you watch?','')
-
-    while(numberOfFilms === '' || numberOfFilms == null || isNaN(numberOfFilms) || numberOfFilms === undefined || numberOfFilms === 0){
-        numberOfFilms = +prompt('How many movies did you watch?','')
-
-    }
-
-}
-function personalRaiting(){
-    if(personalMovieDB.count < 10){
-        userStatus = 'переглянуто досить мало фільмів'
-
-    }else if(personalMovieDB.count >10 && personalMovieDB.count <= 30){
-        userStatus = 'Ви середній глядач'
-    }else if(personalMovieDB.count > 30){
-        userStatus = 'Ви кіноман'
-    }else{
-        console.log('error')
-    }
-
-}
-function showMyDB(){
-    if(!personalMovieDB.private){
-        console.log(personalMovieDB)
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 768,
+    colors: {
+        bg: 'red',
+        border: 'black',
+        test: {
+            man: 'jora',
+        }
     }
 }
-function writeYourGenres(){
-    for(let i = 1; i <=3; i++){
-        genre = prompt('Please enter your favourite genres', '')
-        if(i < 1)
-            i = 1;
-        if(Number.isInteger(genre) || genre == null){
-            i--;
+
+/*
+console.log(options.name)
+*/
+
+
+// delete options.name
+
+// console.log(options)
+
+/*
+for (let option in options){
+    console.log(options)
+}
+*/
+
+/*
+function printObject(object){
+    for (let key in object){
+        if(typeof object[key] === "object"){
+            console.log(`${key}:`)
+            printObject(object[key])
         }else{
-            personalMovieDB.genres.push(genre)
+            console.log("\t" + `${key} => ${object[key]}`)
 
         }
     }
 }
-start()
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false,
-};
+printObject(options)
+*/
 
+Object.keys(options);
 
-do{
-    let lastWatchedMovie = prompt('What movie did you last watch?','')
-    let markMovie = prompt('Please enter its mark', '');
-
-    if(lastWatchedMovie != null && markMovie !== null && lastWatchedMovie !== '' && lastWatchedMovie.length <=  50 && markMovie !== ''){
-        personalMovieDB.movies[lastWatchedMovie] = markMovie
-        console.log('saved movie')
-        i++
-    }else{
-        i--
-        if(i < 1) i = 1
-    }
-}while (i <= 2)
-
-
-personalRaiting()
-showMyDB()
-writeYourGenres()
